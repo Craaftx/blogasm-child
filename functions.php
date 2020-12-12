f1d7f123d80becadf4dde3db6ffc8be4ff8477b7
Guilmain Dorian <guilmain.dorian@gmail.com>
2020-12-12 22:57:15 +0100

    :sparkles: Update single post banner

--- functions.php
+++ functions.php
@@ -37,3 +37,58 @@ if (!function_exists("be_dps_template_part")) {
 	}
 	add_action('display_posts_shortcode_output', 'be_dps_template_part', 10, 2);
 }
+
+if (!function_exists('banner_text_mode_color_add_custom_box')) {
+
+	if (!function_exists('banner_text_mode_color_inner_custom_box')) {
+		function banner_text_mode_color_inner_custom_box($post)
+		{
+			wp_nonce_field(plugin_basename(__FILE__), 'banner_text_mode_color_noncename');
+
+			$value = get_post_meta($post->ID, 'banner_text_mode_color_mode_field') ? get_post_meta($post->ID, 'banner_text_mode_color_mode_field') : 'New Field';
+
+			echo '<label for="banner_text_mode_color_mode_field">';
+			_e("Couleur du texte ", 'banner_text_mode_color_textdomain');
+			echo '</label> ';
+			echo '<select type="text" id="banner_text_mode_color_mode_field" name="banner_text_mode_color_mode_field"><option selected="' . ($value === "light" ? "selected" : null) . '" value="light">Clair</option><option selected="' . ($value === "dark" ? "selected" : null) . '" value="dark">Sombre</option></select>';
+		}
+	}
+
+	function banner_text_mode_color_add_custom_box()
+	{
+		add_meta_box(
+			'banner_text_mode_color_sectionid',
+			__('', 'banner_text_mode_color_textdomain'),
+			'banner_text_mode_color_inner_custom_box',
+			'post'
+		);
+		add_meta_box(
+			'banner_text_mode_color_sectionid',
+			__('Mode de bannière', 'banner_text_mode_color_textdomain'),
+			'banner_text_mode_color_inner_custom_box',
+			'page'
+		);
+	}
+	add_action('add_meta_boxes', 'banner_text_mode_color_add_custom_box');
+}
+
+if (!function_exists('banner_text_mode_color_save_postdata')) {
+	function banner_text_mode_color_save_postdata($post_id)
+	{
+		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+			return;
+		if (!wp_verify_nonce($_POST['banner_text_mode_color_noncename'], plugin_basename(__FILE__)))
+			return;
+		if ('page' == $_POST['post_type']) {
+			if (!current_user_can('edit_page', $post_id))
+				return;
+		} else {
+			if (!current_user_can('edit_post', $post_id))
+				return;
+		}
+
+		$mydata = $_POST['banner_text_mode_color_mode_field'];
+		update_post_meta($post_id, 'banner_text_mode_color_mode_field', $mydata);
+	}
+	add_action('save_post', 'banner_text_mode_color_save_postdata');
+}
